cac995c974c73a42e16d7ce9625d56547a2db5eb
zhangxiaolin198313@163.com <zhangxiaolin198313@163.com>
2023-01-19 07:53:56 +0800

    布局

--- widget.cpp
+++ widget.cpp
@@ -1,6 +1,8 @@
 #include "widget.h"
 #include "./ui_widget.h"
+#include <QList>
 #include <QMessageBox>
+#include <QAbstractButton>
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
@@ -12,7 +14,10 @@ Widget::Widget(QWidget *parent)
     connect(ui->pushButtonClose,&QPushButton::clicked,this,Widget::onCloseClicked);
 
     //绑定主业务处理信号槽处理事件
-
+    QList<QAbstractButton *> listBus=ui->widgetBusiness->findChildren<QAbstractButton *>();
+    foreach (QAbstractButton *btn, listBus) {
+        connect(btn,&QToolButton::clicked,this,Widget::mainMenuClicked);
+    }
 }
 
 Widget::~Widget()
@@ -24,11 +29,10 @@ void Widget::onCloseClicked(){
     msgBox.setText("关闭.");
     msgBox.exec();
 }
-void Widget::onMinClicked(){
+void Widget::onMinClicked() {
     QMessageBox msgBox;
     msgBox.setText("最小化.");
     msgBox.exec();
-
 }
 void Widget::onMaxClicked(){
     QMessageBox msgBox;
@@ -37,5 +41,27 @@ void Widget::onMaxClicked(){
 }
 
 void Widget::mainMenuClicked(){
+    QAbstractButton *btn=(QAbstractButton *)sender();
 
+   QString btnTxt=btn->text();
+   QList<QAbstractButton *> tbtns = ui->widgetBusiness->findChildren<QAbstractButton *>();
+   foreach (QAbstractButton *btn, tbtns) {
+       btn->setChecked(btn == btn);
+   }
+
+   if (btnTxt == "首页") {
+       ui->stackedWidget->setCurrentIndex(0);
+   } else if (btnTxt == "视频") {
+       ui->stackedWidget->setCurrentIndex(1);
+   } else if (btnTxt == "设置") {
+       ui->stackedWidget->setCurrentIndex(2);
+   } else if (btnTxt == "地图") {
+       ui->stackedWidget->setCurrentIndex(3);
+
+} else if (btnTxt == "情报") {
+    ui->stackedWidget->setCurrentIndex(4);
+}
+   else if (btnTxt == "用户退出") {
+       exit(0);
+   }
 }
