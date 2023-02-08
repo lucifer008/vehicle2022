09782005d9c600ff583bbbad0fcf3a7a07124c97
lucifer008 <zhangxiaolin198313@163.com>
2023-02-08 12:02:45 +0800

    资源文件测试

--- main.cpp
+++ main.cpp
@@ -4,7 +4,15 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setAttribute(Qt::AA_Use96Dpi);
+    QApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::Floor);
     QApplication a(argc, argv);
+    QFont font;
+    //设置字体
+    font.setFamily("Microsoft Yahei");
+    font.setPixelSize(13);
+    a.setFont(font);
+
     Widget w;
     w.show();
     return a.exec();
